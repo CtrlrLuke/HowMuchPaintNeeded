239b44a64c54b45eff4e122364b162686ad4f8ce
Luke Pritchard <Lukepritchard04@gmail.com>
2025-02-07 11:30:51 -0500

    Added fence dimension input

--- HowMuchPaintNeeded.cpp
+++ HowMuchPaintNeeded.cpp
@@ -8,23 +8,30 @@ using namespace std;
 int main()
 {
 	// name variables
-	double paintSQFTPerGallon, fenceSQFT, paintNeeded;
+	double paintSQFTPerGallon, fenceSQFT, paintNeeded, fenceLength, fenceHeight;
 
 	// define variables
 	paintSQFTPerGallon = 340;
 
+	// gather fence dimensions
+	cout << "Enter the lenght of your fence in feet: " ;
+	cin >> fenceLength;
+
+	cout << "\nEnter the height of your fence in feet: ";
+	cin >> fenceHeight;
+
 	// make sure two coats, double SQFT
-	fenceSQFT = 2 * (6 * 100);
+	fenceSQFT = (fenceHeight * fenceLength) * 2;
 
 	// calculate exact number of gallons needed
-	paintNeeded = fenceSQFT / paintSQFTPerGallon;
+	paintNeeded = (fenceSQFT / paintSQFTPerGallon);
 
 	// round up to nearest whole number using ceil()
 	int paintNeededRoundedUp = static_cast<int>(ceil(paintNeeded));
 
 	// display needed paint
-	cout << "You need " << paintNeededRoundedUp << " gallons of paint for two coats on a 6x100ft fence." << endl;
-	cout << "\nHello";
+	cout << "You need " << paintNeededRoundedUp << " gallons of paint for two coats on a " << fenceHeight << "x" << fenceLength << "ft fence." << endl;
+	
 	return 0;
 }
 
